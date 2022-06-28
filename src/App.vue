92ab4c6806ea7332c3a4abee924dbfa2809b7c1a
Samuel Breuillet <breuillet.samuel@gmail.com>
2022-06-28 03:07:05 +0200

    Feature(Pixel): create board

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,75 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <div id="colors">
+            <div
+                v-for="color in colors"
+                :key="color"
+                :style="`background-color: ${color}`"
+                @click.prevent="setSelectedColor(color)"
+            >
+            </div>
+        </div>
+        
+        <div id="pixelArt">
+            <Pixel
+                v-for="n in 121"
+                :key="n"
+                :selected-color="selectedColor"
+                :default-color="defaultColor"
+                ref="pixel"
+            ></Pixel>
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Pixel from "@/components/Pixel";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    components: {
+        Pixel
+    },
+    data() {
+        return {
+            colors: ['#FC0000', '#FFD602', '#238B21', '#1C90FF', '#000'],
+            defaultColor: '#1C90FF',
+            selectedColor: null
+        }
+    },
+    created() {
+        this.selectedColor = this.colors[0]
+    },
+    methods: {
+        applyColor(n) {
+            console.log(this.selectedColor)
+            this.$refs.pixel[n].setColor(this.selectedColor)
+        },
+        setSelectedColor(color) {
+            console.log(color)
+            this.selectedColor = color
+        }
+    },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#colors {
+    display: grid;
+    grid-template-columns: repeat(12, 50px);
+    grid-auto-rows: minmax(50px, auto);
+}
+
+#pixelArt {
+    display: grid;
+    grid-template-columns: repeat(11, 50px);
+    grid-auto-rows: minmax(50px, auto);
+    grid-gap: 1px;
+    background-color: black;
+    width: 561px;
+}
+
+#pixelArt div {
+    border: solid black 1px;
 }
 </style>
---
+++ src/components/Pixel.vue
@@ -0,0 +1,31 @@
+<template>
+    <div
+        :style="`background-color: ${color}`"
+        @click.prevent="setColor(selectedColor)"
+        @mouseover="setColor(selectedColor)"
+        @mouseleave="setColor(defaultColor)"
+    >
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'PixelVue',
+    props:['selectedColor', 'defaultColor'],
+    data() {
+        return {
+            color: null,
+        }
+    },
+    created() {
+        this.color = this.defaultColor
+    },
+    methods: {
+        setColor(color){
+            console.log('color')
+            this.color = color
+        }
+    }
+    
+}
+</script>
